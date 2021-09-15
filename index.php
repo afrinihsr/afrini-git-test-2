324854b5b40ce5c14629bbeb431925e165ac01cf
hafsahafrini@gmail.com <hafsahafrini@gmail.com>
2021-09-15 11:30:09 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Coba git Remote</title>
+    <title>Coba Git Remote</title>
 </head>
 <body>
+    <h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi quod repellat, sit veritatis eaque inventore? Nostrum exercitationem quam maiores, voluptatum et quod itaque necessitatibus amet sunt adipisci fugiat quia dolore.</p>
     
 </body>
 </html>
